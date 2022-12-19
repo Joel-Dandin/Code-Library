83694ba6f96f9e9d442240929fc248339a17b7f2
Fedric <60149879+Joel-Fedric@users.noreply.github.com>
2022-12-19 23:50:36 +0530

    Create Catalan numbers.py

---
+++ Python/Catalan numbers.py
@@ -0,0 +1,12 @@
+def ncatalan(n):
+    if n <= 1:
+        return 1
+    result = 0
+    for i in range(n):
+        result += ncatalan(i) * ncatalan(n-i-1)
+ 
+    return result
+ 
+print("The 20 catalan numbers are\n")
+for i in range(15):
+    print(ncatalan(i), end=" ")
